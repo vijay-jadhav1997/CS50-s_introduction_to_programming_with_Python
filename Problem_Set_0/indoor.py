76bc17b983ec618496a66bb8246513ba3c5d96df
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-06 19:24:08 +0530

    try to solve Indoor Voice problem

---
+++ Problem_Set_0/indoor.py
@@ -0,0 +1,9 @@
+# Get user input
+user_inp = input('Enter here, what\'s in your mind. => ')
+
+# transform user_inp to lowercase
+user_inp = user_inp.lower()
+
+# display user_inp to screen
+
+print(user_inp)
