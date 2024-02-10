c7e6a4a461c5c26fb6f627b8c1cb7b212c7481e8
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-10 20:26:09 +0530

    learn 'while' loop in Python and solve 'coke' problem

---
+++ Problem_Set_2/coke.py
@@ -0,0 +1,20 @@
+# define main Fn
+def main():
+  change_owed = 0
+  amount_due = 50
+  prompt_user(change_owed, amount_due)
+
+# define prompt_user fn
+def prompt_user(change, amount):
+
+  while amount > 0 :
+    print(f"Amount Due: {amount}")
+    user_inp = int(input("Insert Coin: "))
+    if user_inp == 5 or user_inp == 10 or user_inp == 25:
+      amount -= user_inp
+  change = abs(amount)
+  print(f"Change Owed: {change}")
+
+# call main fn
+if __name__ == "__main__":
+  main()
