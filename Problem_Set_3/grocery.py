f17026fcbffcaf88686b65ce5837cdc068fd4544
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-02-11 19:48:52 +0530

    learn function in Python and solve 'Grocery List' problem

---
+++ Problem_Set_3/grocery.py
@@ -0,0 +1,27 @@
+def main():
+  create_list()
+
+
+
+
+def create_list():
+  items = {}
+  while True:
+    try:
+      item = input("").upper()
+    except EOFError:
+      items = {item: items[item]  for item in sorted(items)}
+      for item in items:
+        print(f"{items[item]} {item}")
+      break
+    except KeyError:
+      pass
+    else:
+      if item in items :
+        items[item] += 1
+      else:
+        items[item] = 1
+
+# call main Fn
+if __name__ == "__main__":
+  main()
